d348ebc7a58b9fad8a0c3e6034723724958a35ab
agathe <agathe@agat.dev>
2026-02-08 05:08:47 +0100

    clarity dashboard

--- components/clarity-script.tsx
+++ components/clarity-script.tsx
@@ -1,13 +1,18 @@
 'use client'
 
-import { useEffect } from 'react'
-
 export function ClarityScript() {
-  useEffect(() => {
-    import('@microsoft/clarity').then((clarity) => {
-      clarity.default.init('vdyvivixlr')
-    })
-  }, [])
-
-  return null
+  return (
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vdyvivixlr");
+        `,
+      }}
+    />
+  )
 }
